2bb62cd5c056f71c506e04937bfe176ea6371665
Rahul Biswakarma <rahul.id39@gmail.com>
2023-01-06 15:21:12 +0530

    Added bg changer

--- scripts/slide.js
+++ scripts/slide.js
@@ -9,6 +9,8 @@ var slideBaseClass = [
   "transition-all",
 ];
 
+var slides = document.getElementsByClassName("slide");
+
 function updateSlides() {
   var slides = document.getElementsByClassName("slide");
   for (var i = 0; i < slides.length; i++) {
@@ -24,17 +26,26 @@ function updateSlides() {
 
     //   Add slide text
     slides[i].innerHTML += `
-        <div class="max-w-[800px] h-80% flex items-center flex-col gap-[5rem] justify-center">
-            <h1 id="h${i + 1}" class="text-9xl font-bold">Slide ${i + 1}</h1>
-            <div class="flex flex-col gap-[1rem] bg-white/30 px-[30%] py-[10%] transition-all">
-                <input class="border-b-2 border-black p-[1rem] transition-all" type="text" name="input0" id="input${
+        <div class="max-w-[800px] h-80% flex items-center justify-center">
+        
+            <div class="flex items-center flex-col gap-[5rem] justify-center">
+                <h1 id="h${i + 1}" class="text-9xl font-bold">Slide ${
+      i + 1
+    }</h1>
+                <div class="flex flex-col gap-[1rem] bg-white/30 px-[30%] py-[10%] transition-all">
+                    <input class="border-b-2 border-black p-[1rem] transition-all" type="text" name="input0" id="input${
+                      i + 1
+                    }"
+                        value="Slide ${i + 1}" placeholder="Slide ${i + 1}" />
+                    <button onclick="updateText(${i + 1})"
+                        class="font-bold bg-black/60 px-[2rem] py-[0.5rem] text-white transition-all">
+                        Update
+                    </button>
+                </div>
+                <div class="flex gap-[10px] bg-white/30 px-[30%] py-[10%] transition-all">Change Color
+                <input type="color" name="color" id="color${
                   i + 1
-                }"
-                    value="Slide ${i + 1}" placeholder="Slide ${i + 1}" />
-                <button onclick="updateText(${i + 1})"
-                    class="font-bold bg-black/60 px-[2rem] py-[0.5rem] text-white transition-all">
-                    Update
-                </button>
+                }" onchange="changeColor(${i + 1})"></div>
             </div>
         </div>
         `;
@@ -44,3 +55,9 @@ function updateSlides() {
 window.onload = function () {
   updateSlides();
 };
+
+function changeColor(i) {
+  slides[i - 1].style.backgroundColor = document.getElementById(
+    `color${i}`
+  ).value;
+}
